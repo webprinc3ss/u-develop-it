204675ba4ee190f7b839cdca5641a8a29cc308e6
Mystic Kitty <djabranton@gmail.com>
2020-12-06 19:27:38 -0800

    Created parties table and left-joined to candidates.

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 // Get all candidates 
 app.get('/api/candidates', (req, res) => {
-  const sql =  `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
   const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
@@ -37,8 +41,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates 
-               WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id 
+  WHERE candidates.id = ?`;
   const params = [req.params.id];
   db.get(sql, params, (err, row) => {
     if (err) {
@@ -61,11 +69,11 @@ app.post('/api/candidate', ({ body }, res) => {
     return;
   }
 
-  const sql =  `INSERT INTO candidates (first_name, last_name, industry_connected) 
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
                 VALUES (?,?,?)`;
   const params = [body.first_name, body.last_name, body.industry_connected];
   // ES5 function, not arrow function, to use this
-  db.run(sql, params, function(err, result) {
+  db.run(sql, params, function (err, result) {
     if (err) {
       res.status(400).json({ error: err.message });
       return;
@@ -83,7 +91,7 @@ app.post('/api/candidate', ({ body }, res) => {
 app.delete('/api/candidate/:id', (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
   const params = [req.params.id]
-  db.run(sql, params, function(err, result) {
+  db.run(sql, params, function (err, result) {
     if (err) {
       res.status(400).json({ error: res.message });
       return;
@@ -96,7 +104,7 @@ app.delete('/api/candidate/:id', (req, res) => {
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
   res.status(404).end();
-}); 
+});
 
 // Start server after DB connection
 db.on('open', () => {
