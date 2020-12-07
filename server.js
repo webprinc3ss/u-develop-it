62737406004bb9fcd5f2ea39154e4783224b1567
Mystic Kitty <djabranton@gmail.com>
2020-12-06 16:54:48 -0800

    Added post method to server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const sqlite3 = require('sqlite3').verbose();
 //Import Express
 const express = require('express');
 
+//Needed for Post
+const inputCheck = require('./utils/inputCheck');
+
 //PORT designation and the app expression
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -72,6 +75,32 @@ app.delete('/api/candidate/:id', (req, res) => {
             changes: this.changes
         });
     });
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+
 });
 
 // Create a candidate
